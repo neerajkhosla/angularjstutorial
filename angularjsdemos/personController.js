a24d48fa271832922116d9a3eed7811a78a79476
neeraj khosla <meet.best@gmail.com>
2015-10-17 08:22:34 +0530

    play with angularjs - repository created.

---
+++ angularjsdemos/personController.js
@@ -0,0 +1,10 @@
+﻿angular.module('myApp', []).controller('namesCtrl', function ($scope) {
+    $scope.names = [
+        { name: 'Mohan', country: 'India' },
+        { name: 'Neeraj', country: 'USA' },
+        { name: 'Nitin', country: 'UK' },
+        { name: 'Ravinder', country: 'Sweden' },
+        { name: 'Rohit', country: 'Sweden' },
+        { name: 'Navjeet', country: 'Germany' },
+    ];
+});
